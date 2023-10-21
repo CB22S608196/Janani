91f5b3a73ac904ef35ca7900d70f19f4eafa7ef5
CB22S608196 <145000898+CB22S608196@users.noreply.github.com>
2023-10-21 21:47:31 +0530

    Add files via upload

---
+++ main (12).py
@@ -0,0 +1,19 @@
+
+
+'''Linear_search_product'''
+
+def linearSearchProduct (productList,targetProduct):
+  indices = []
+
+  for index, product in enumerate(productList):
+   if product == targetProduct:
+     indices.append(index)
+     
+     return indices
+
+  #Example usage:
+product = ["shoes",       "boot", "Loafer", "shoes","sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'  
+result = linearSearchProduct(product,target)
+print(result)
---
+++ main (13).py
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+ #Define the base class player
+class player: 
+  def play(self):
+      print("The player is playing cricket.")
+    
+# Define the derived class Batsman
+class Batsman(player):
+      def play(self):
+        print("The batsman is battling.")
+        
+# Define the derived class Bowler
+class Bowler(player):
+  def play(self):
+    print("The bowler is bowling.")
+    
+#  Create objects of Batsman and Bowler
+batsman = Batsman()
+bowler = Bowler()
+
+# Call the play() method for each object
+batsman.play()
+bowler.play()
+
+  
+      
